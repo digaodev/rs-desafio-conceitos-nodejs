1c339b9c03b5f1e6af0137c90fd7a364b61346a9
digaodev <rodrigo.rb.dev@gmail.com>
2020-04-26 18:05:21 -0300

    feat: implement all basic route functionalities.

--- src/app.js
+++ src/app.js
@@ -1,33 +1,83 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const repositories = [];
+let repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs = [] } = request.body;
+
+  const newRepo = { id: uuid(), title, url, techs, likes: 0 };
+
+  repositories.push(newRepo);
+
+  return response.json(newRepo);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs = [] } = request.body;
+  const { id } = request.params;
+
+  const originalRepoIndex = repositories.findIndex((repo) => repo.id === id);
+
+  if (originalRepoIndex === -1) {
+    return response.status(400).send();
+  }
+
+  const originalRepo = repositories.find((repo) => repo.id === id);
+  const updatedRepo = { ...originalRepo, title, url, techs };
+
+  repositories = [
+    ...repositories.slice(0, originalRepoIndex),
+    updatedRepo,
+    ...repositories.slice(originalRepoIndex + 1),
+  ];
+
+  return response.json(updatedRepo);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex((repo) => repo.id === id);
+
+  if (repoIndex === -1) {
+    return response.status(400).send();
+  }
+  repositories = repositories.filter((repo) => repo.id !== id);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const originalRepoIndex = repositories.findIndex((repo) => repo.id === id);
+  const originalRepo = repositories.find((repo) => repo.id === id);
+
+  if (!originalRepo) {
+    return response.status(400).send();
+  }
+
+  const updatedRepo = { ...originalRepo, likes: originalRepo.likes + 1 };
+
+  repositories = [
+    ...repositories.slice(0, originalRepoIndex),
+    updatedRepo,
+    ...repositories.slice(originalRepoIndex + 1),
+  ];
+
+  return response.json(updatedRepo);
 });
 
 module.exports = app;
